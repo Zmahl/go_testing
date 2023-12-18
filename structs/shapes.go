11018b5cd5a4987fe663544217d92b0801cac252
Zmahl <zach.mahl@udel.edu>
2023-12-18 09:30:59 -0500

    Table driven tests

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,6 +1,21 @@
 package structs
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
+	width  float64
+	height float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+type Triangle struct {
 	width  float64
 	height float64
 }
@@ -9,6 +24,14 @@ func Perimeter(rect Rectangle) float64 {
 	return 2 * (rect.width + rect.height)
 }
 
-func Area(rect Rectangle) float64 {
+func (rect Rectangle) Area() float64 {
 	return rect.width * rect.height
+}
+
+func (circle Circle) Area() float64 {
+	return math.Pi * circle.radius * circle.radius
+}
+
+func (triangle Triangle) Area() float64 {
+	return 0
 }
