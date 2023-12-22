c18570673c1bc835796eebbd27d5fdd0781cb695
Zmahl <zachmahl@udel.edu>
2023-12-22 11:45:35 -0500

    added the result struct and channel

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -3,6 +3,10 @@ package concurrency
 import "time"
 
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
